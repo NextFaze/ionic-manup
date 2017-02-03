94e5a0431a6c1a3e8fc9d44047416607e06f564b
Michael Marner <mmarner@nextfaze.com>
2017-02-03 13:30:31 +1030

    Remove hard coded app name, urls.

--- src/manup.config.ts
+++ src/manup.config.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ManupConfig {
-    url: String
+    url: string
 }
--- src/manup.service.ts
+++ src/manup.service.ts
@@ -2,18 +2,27 @@ import { ManupConfig } from './manup.config';
 import { AlertController, Platform } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Injectable, Optional } from '@angular/core';
-import { AppVersion } from 'ionic-native';
+import { AppVersion, InAppBrowser } from 'ionic-native';
 
 import * as semver from 'semver';
 
 @Injectable()
 export class ManupService {
 
-    public constructor(private http: Http, private alert: AlertController, private platform: Platform, @Optional() private storage: Storage, private config: ManupConfig) {}
+    public constructor(private http: Http, private alert: AlertController, private platform: Platform, private config: ManupConfig) {}
 
+    /**
+     * True if there is an alert already displayed. Used to prevent multiple alerts 
+     * being presented on top of one another
+     */
     private inProgress: boolean = false; 
+
+    /**
+     * A reference to the current unresolved promise
+     */
     private currentPromise: Promise<any>;
     
+
     /**
      * Begins the manup check process.
      * 
@@ -24,9 +33,13 @@ export class ManupService {
         if (!this.inProgress) {
             this.inProgress = true;
             this.currentPromise = new Promise( (resolve, reject) => {
+                console.log('waiting for platform');
                 this.platform.ready()
                 .then( () => {
-                    this.http.get(this.config.url + '?q=' + Math.random()).subscribe(response => {
+                    console.log('fetching remote manup file: ' + this.config.url)
+                    this.http.get(this.config.url).subscribe(response => {
+                        console.log('got response: ')
+                        console.log(response.text());
                         this.evaluate(response.json()).then( () => {
                             this.inProgress = false;
                             resolve();
@@ -34,6 +47,7 @@ export class ManupService {
                     },
                     // Let the app run if we can't get the remote file
                     error => {
+                        console.log('could not fetch manup metadata');
                         this.inProgress = false;
                         resolve();
                     });
@@ -46,20 +60,36 @@ export class ManupService {
     /**
      * Evaluates the app version against manup data, presents alerts as needed.
      */
-    private evaluate(versionJson: any): Promise<any> {
+    private evaluate(metadata: any): Promise<any> {
+        let platformData: {minimum: string, latest: string, link: string};
 
-        if (versionJson.disabled) {
+        if (this.platform.is('ios')) {
+            platformData = metadata.ios;
+        }
+        else if (this.platform.is('android')) {
+            platformData = metadata.android;
+        }
+        else {
+            return Promise.resolve(true);
+        }
+
+        if (metadata.disabled) {
             return this.presentMaintenanceMode()
         }
         else {
             return AppVersion.getVersionNumber().then(version => {
-                if (semver.lt(version, versionJson.minimum)) {
-                    return this.presentMandatoryUpdate()
+                try {
+                    if (semver.lt(version, platformData.minimum)) {
+                        return this.presentMandatoryUpdate(platformData)
+                    }
+                    else if (semver.lt(version, platformData.latest)) {
+                        return this.presentOptionalUpdate(platformData)
+                    }
+                    else {
+                        return Promise.resolve(true);
+                    }
                 }
-                else if (semver.lt(version, versionJson.latest)) {
-                    return this.presentOptionalUpdate()
-                }
-                else {
+                catch(e) {
                     return Promise.resolve(true);
                 }
             });
@@ -72,14 +102,16 @@ export class ManupService {
      * @returns a promise that will never resolve, because the app should not continue
      */
     presentMaintenanceMode(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            let alert = this.alert.create({
-                enableBackdropDismiss: false,
-                title: "App Unavailable",
-                subTitle: "Elders Grain is currently unavailable, please check back again later.",
-            })
-            alert.present();
-        });
+        return AppVersion.getAppName().then( name => {
+            return new Promise((resolve, reject) => {
+                let alert = this.alert.create({
+                    enableBackdropDismiss: false,
+                    title: "App Unavailable",
+                    subTitle: `${name} is currently unavailable, please check back again later.`,
+                })
+                alert.present();
+            });
+        })
     }
 
     /**
@@ -87,28 +119,25 @@ export class ManupService {
      * 
      * @returns a promise that will never resolve, because the app should not continue
      */
-    presentMandatoryUpdate(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            let alert = this.alert.create({
-                enableBackdropDismiss: false,
-                title: "Update Required",
-                subTitle: "An update to Elders Grain is required to continue.",
-                buttons: [
-                    {
-                        text: 'Update',
-                        handler: () => {
-                            if (this.platform.is('ios')) {
-                              //new InAppBrowser("https://itunes.apple.com/au/app/elders-grain/id1145184771?mt=8", '_system');
+    presentMandatoryUpdate(platformData: any): Promise<any> {
+        return AppVersion.getAppName().then( name => {
+            return new Promise((resolve, reject) => {
+                let alert = this.alert.create({
+                    enableBackdropDismiss: false,
+                    title: "Update Required",
+                    subTitle: `An update to ${name} is required to continue.`,
+                    buttons: [
+                        {
+                            text: 'Update',
+                            handler: () => {
+                                new InAppBrowser(platformData.link, '_system');
+                                return false;
                             }
-                            else {
-                              //new InAppBrowser("market://details?id=com.elders.grain", '_system');
-                            }
-                            return false;
                         }
-                    }
-                ]
-            })
-            alert.present();
+                    ]
+                })
+                alert.present();
+            });
         });
     }
 
@@ -117,34 +146,31 @@ export class ManupService {
      * 
      * @returns a promise that will resolves if the user selects 'not now'
      */
-    presentOptionalUpdate(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            let alert = this.alert.create({
-                enableBackdropDismiss: false,
-                title: "Update Available",
-                subTitle: "An update to Elders Grain is available. Would you like to update?",
-                buttons: [
-                    {
-                        text: 'Not Now',
-                        handler: () => {
-                            resolve();
-                        }
-                    },
-                    {
-                        text: 'Update',
-                        handler: () => {
-                            if (this.platform.is('ios')) {
-                              //new InAppBrowser("https://itunes.apple.com/au/app/elders-grain/id1145184771?mt=8", '_system');
+    presentOptionalUpdate(platformData: any): Promise<any> {
+        return AppVersion.getAppName().then( name => {
+            return new Promise((resolve, reject) => {
+                let alert = this.alert.create({
+                    enableBackdropDismiss: false,
+                    title: "Update Available",
+                    subTitle: `An update to ${name} is available. Would you like to update?`,
+                    buttons: [
+                        {
+                            text: 'Not Now',
+                            handler: () => {
+                                resolve();
                             }
-                            else {
-                              //new InAppBrowser("market://details?id=com.elders.grain", '_system');
+                        },
+                        {
+                            text: 'Update',
+                            handler: () => {
+                                new InAppBrowser(platformData.link, '_system');
+                                return false;
                             }
-                            return false;
                         }
-                    }
-                ]
-            })
-            alert.present();
+                    ]
+                })
+                alert.present();
+            });
         });
     }
 }
