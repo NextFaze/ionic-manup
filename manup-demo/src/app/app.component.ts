16e6e468ce674443120ad1200ec17ec606308e6f
Michael Marner <mmarner@nextfaze.com>
2017-02-03 13:31:03 +1030

    Manup demo ionic application.

---
+++ manup-demo/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { ManupService } from 'ionic-manup';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage = TabsPage;
+
+  constructor(platform: Platform, private manup: ManupService) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+
+      manup.versionCheck();
+
+    });
+  }
+}
---
+++ manup-demo/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ManupModule, ManupService } from 'ionic-manup';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    ManupModule.forRoot({url: 'https://apps.eldersapps.com.au/manup/com.elders.grain.json'})
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
+    ManupService
+  ]
+})
+export class AppModule {}
