e299ab7566bac622fe596e44bced0800f15e6a24
Michael Marner <mmarner@nextfaze.com>
2017-03-10 17:16:22 +1030

    Set default languate to english

--- manup-demo/src/app/app.component.ts
+++ manup-demo/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { ManUpService } from 'ionic-manup';
+import { TranslateService } from 'ng2-translate'
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +13,8 @@ import { ManUpService } from 'ionic-manup';
 export class MyApp {
   rootPage = TabsPage;
 
-  constructor(platform: Platform, private manup: ManUpService) {
+  constructor(platform: Platform, private manup: ManUpService, private translate: TranslateService) {
+    translate.setDefaultLang('en');
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
