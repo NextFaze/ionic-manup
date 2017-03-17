678ecb074979a1a5ac6625c22948d65cfcfa6fbe
Michael Marner <mmarner@nextfaze.com>
2017-03-17 15:45:26 +1030

    Internal translation objects
    Save the app devs from having to do it

---
+++ src/i18n/en.ts
@@ -0,0 +1,24 @@
+
+export var en = {
+    lang: 'en',
+    translations: {
+        manup: {
+            mandatory: {
+                title: "Update Required",
+                text: "An update to {{app}} is required to continue.",
+            },
+            optional: {
+                title: "Update Available",
+                text: "An update to {{app}} is available. Would you like to update?"
+            },
+            maintenance: {
+                title: "{{app}} Unavailable",
+                text: "{{app}} is currently unavailable, please check back again later."
+            },
+            buttons: {
+                update: "Update",
+                later: "Not Now"
+            }
+        }
+    }
+}
