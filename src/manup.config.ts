25657bf13a306e9270453d61bce70b2b8cd8fb55
Michael Marner <mmarner@nextfaze.com>
2017-04-21 17:42:19 +0930

    Document config params

--- src/manup.config.ts
+++ src/manup.config.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
+/**
+ * Configuration values for ManUp
+ */
 export class ManUpConfig {
+    /**
+     * The metadata URL to read version info from.
+     * 
+     * @type {string}
+     * @memberOf ManUpConfig
+     */
     url: string
+
+    /**
+     * Whether to assume translations will be provided to the module.
+     * 
+     * @type {boolean}
+     * @memberOf ManUpConfig
+     */
     externalTranslations?: boolean;
 }
