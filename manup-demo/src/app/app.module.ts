09c6173dae6928e16a57e1792dedcc2e04dcfb4d
Michael Marner <michael@20papercups.net>
2017-10-09 15:46:28 +1030

    Fix Ionic native providers

--- manup-demo/src/app/app.module.ts
+++ manup-demo/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppVersion } from '@ionic-native/app-version';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -14,7 +20,9 @@ export function translateLoader(http: HttpClient) {
 @NgModule({
   declarations: [MyApp, HomePage],
   imports: [
+    BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule,
     HttpClientModule,
     ManUpModule.forRoot({
       url: 'https://raw.githubusercontent.com/NextFaze/ionic-manup/master/manup-demo/manup.json',
@@ -29,7 +37,14 @@ export function translateLoader(http: HttpClient) {
     })
   ],
   bootstrap: [IonicApp],
-  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, ManUpService],
+  providers: [
+    AppVersion,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ManUpService,
+    SplashScreen,
+    InAppBrowser,
+    StatusBar
+  ],
   entryComponents: [MyApp, HomePage]
 })
 export class AppModule {}
