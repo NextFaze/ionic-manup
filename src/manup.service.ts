5338dbfacad606accbc6e5924beb14c789dce39d
Michael Marner <michael@20papercups.net>
2017-10-09 14:53:58 +1030

    Fix use of InAppBrowser

--- src/manup.service.ts
+++ src/manup.service.ts
@@ -1,175 +1,174 @@
-import { TranslateService } from '@ngx-translate/core';
-import { ManUpConfig } from './manup.config';
-import { AlertController, Platform } from 'ionic-angular';
-import { Http } from '@angular/http';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/map';
+
 import { Injectable, Optional } from '@angular/core';
+import { Http } from '@angular/http';
 import { AppVersion } from '@ionic-native/app-version';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
-
-import { i18n } from './i18n';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/fromPromise';
-
+import { TranslateService } from '@ngx-translate/core';
+import { AlertController, Platform } from 'ionic-angular';
+import { Observable } from 'rxjs';
 import * as semver from 'semver';
 
-const STORAGE_KEY='com.nextfaze.ionic-manup';
+import { i18n } from './i18n';
+import { ManUpConfig } from './manup.config';
+
+const STORAGE_KEY = 'com.nextfaze.ionic-manup';
 
 /**
  * The types of alerts we may present
  */
 export enum AlertType {
-    /**
+  /**
      * A mandatory update is required
      */
-    MANDATORY,
+  MANDATORY,
 
-    /**
+  /**
      * An optional update is available
      */
-    OPTIONAL,
+  OPTIONAL,
 
-    /**
+  /**
      * The app is disabled
      */
-    MAINTENANCE,
+  MAINTENANCE,
 
-    /**
+  /**
      * Nothing to see here
      */
-    NOP
+  NOP
 }
 
 export interface PlatformData {
-    minimum: string,
-    latest: string,
-    url: string,
-    enabled: boolean
+  minimum: string;
+  latest: string;
+  url: string;
+  enabled: boolean;
 }
 
 /**
  * What the metadata object should look like
  */
 export interface ManUpData {
-    ios?: PlatformData;
-    android?: PlatformData;
-    windows?: PlatformData;
+  ios?: PlatformData;
+  android?: PlatformData;
+  windows?: PlatformData;
 }
 
 @Injectable()
 export class ManUpService {
-    /**
+  /**
      * A local reference to Ionic Native's AppVersion class.
      * Stored locally so mocks can be injected in for testing.
      * 
      * @type {*}
      * @memberOf ManUpService
      */
-    public AppVersion: any = AppVersion;
+  public AppVersion: any = AppVersion;
 
-    public constructor(private config: ManUpConfig, 
-                       private http: Http, 
-                       private alert: AlertController, 
-                       private platform: Platform, 
-                       @Optional() private translate: TranslateService,
-                       @Optional() private storage: Storage) {
-
-        // load the translations unless we've been told not to
-        if (this.translate && !this.config.externalTranslations) {
-            for (let lang of i18n) {
-                this.translate.setTranslation(lang.lang, lang.translations, true);
-            }
-        }
+  public constructor(
+    private config: ManUpConfig,
+    private http: Http,
+    private alert: AlertController,
+    private platform: Platform,
+    private iab: InAppBrowser,
+    @Optional() private translate: TranslateService,
+    @Optional() private storage: Storage
+  ) {
+    // load the translations unless we've been told not to
+    if (this.translate && !this.config.externalTranslations) {
+      for (let lang of i18n) {
+        this.translate.setTranslation(lang.lang, lang.translations, true);
+      }
     }
+  }
 
-    /**
+  /**
      * True if there is an alert already displayed. Used to prevent multiple alerts 
      * being presented on top of one another
      */
-    private inProgress: boolean = false; 
+  private inProgress: boolean = false;
 
-    /**
+  /**
      * A reference to the current unresolved promise
      */
-    private currentPromise: Promise<any>;
+  private currentPromise: Promise<any>;
 
-    /**
+  /**
      * Begins the manup check process.
      * 
      * @Returns a promise that resolves if the app is able to continue.
      */
-    public validate(): Promise<any> {
-
-        if (!this.inProgress) {
-            this.inProgress = true;
-            this.currentPromise = new Promise( (resolve, reject) => {
-                this.platform.ready()
-                .then( () => {
-                    this.metadata()
-                    .map(response => this.getPlatformData(response))
-                    .subscribe(metadata => {
-                        this.evaluate(metadata).then(alert => {
-                            switch (alert) {
-                                case AlertType.NOP:
-                                    resolve();
-                                    break;
-                                default:
-                                    return this.presentAlert(alert, metadata);
-                            }
-                        },
-                        error => resolve());
-                    });
-                })
+  public validate(): Promise<any> {
+    if (!this.inProgress) {
+      this.inProgress = true;
+      this.currentPromise = new Promise((resolve, reject) => {
+        this.platform.ready().then(() => {
+          this.metadata()
+            .map(response => this.getPlatformData(response))
+            .subscribe(metadata => {
+              this.evaluate(metadata).then(
+                alert => {
+                  switch (alert) {
+                    case AlertType.NOP:
+                      resolve();
+                      break;
+                    default:
+                      return this.presentAlert(alert, metadata);
+                  }
+                },
+                error => resolve()
+              );
             });
-        }
-        return this.currentPromise;
+        });
+      });
     }
+    return this.currentPromise;
+  }
 
-    /**
+  /**
      * Evaluates what kind of update is required, if any.
      * 
      * Returns a promise that resolves with an alert type.
      */
-    public evaluate(metadata: PlatformData): Promise<AlertType> {
-        if (!metadata.enabled) {
-            return Promise.resolve(AlertType.MAINTENANCE);
-        }
-        return this.AppVersion.getVersionNumber().then((version:string) => {
-
-            if (semver.lt(version, metadata.minimum)) {
-                return AlertType.MANDATORY;
-            } 
-            else if (semver.lt(version, metadata.latest)) {
-                return AlertType.OPTIONAL;
-            }
-            return AlertType.NOP;
-        });
+  public evaluate(metadata: PlatformData): Promise<AlertType> {
+    if (!metadata.enabled) {
+      return Promise.resolve(AlertType.MAINTENANCE);
     }
+    return this.AppVersion.getVersionNumber().then((version: string) => {
+      if (semver.lt(version, metadata.minimum)) {
+        return AlertType.MANDATORY;
+      } else if (semver.lt(version, metadata.latest)) {
+        return AlertType.OPTIONAL;
+      }
+      return AlertType.NOP;
+    });
+  }
 
-
-    /**
+  /**
      * Fetches the remote metadata and returns an observable with the json
      */
-    public metadata(): Observable<ManUpData> {
-        return this.http.get(this.config.url).map(response => response.json())
-        .map(response => {
-            if (this.storage) {
-                this.saveMetadata(response).catch( () => {});
-            }
-            return response;
-        })
-        .catch(err => {
-            if (this.storage) {
-                return this.metadataFromStorage();
-            }
-            return err;
-        });
-    }
+  public metadata(): Observable<ManUpData> {
+    return this.http
+      .get(this.config.url)
+      .map(response => response.json())
+      .map(response => {
+        if (this.storage) {
+          this.saveMetadata(response).catch(() => {});
+        }
+        return response;
+      })
+      .catch(err => {
+        if (this.storage) {
+          return this.metadataFromStorage();
+        }
+        return err;
+      });
+  }
 
-
-    /**
+  /**
      * Gets the version metadata from storage, if available.
      * 
      * @private
@@ -178,16 +177,17 @@ export class ManUpService {
      * 
      * @memberOf ManUpService
      */
-    metadataFromStorage(): Observable<ManUpData> {
-        if (this.storage) {
-            return Observable.fromPromise((<Promise<string>> this.storage.get(STORAGE_KEY + '.manup'))).map(v=> JSON.parse(v));
-        }
-        else {
-            throw new Error('Storage not configured');
-        }
+  metadataFromStorage(): Observable<ManUpData> {
+    if (this.storage) {
+      return Observable.fromPromise(<Promise<string>>this.storage.get(
+        STORAGE_KEY + '.manup'
+      )).map(v => JSON.parse(v));
+    } else {
+      throw new Error('Storage not configured');
     }
+  }
 
-    /**
+  /**
      * 
      * Saves the metadata to storage.
      * 
@@ -198,33 +198,31 @@ export class ManUpService {
      * 
      * @memberOf ManUpService
      */
-    public saveMetadata(metadata: ManUpData): Promise<any> {
-        if (this.storage) {
-            return this.storage.set(STORAGE_KEY + '.manup', JSON.stringify(metadata));
-        }
-        else {
-            throw new Error('Storage not configured');
-        }
+  public saveMetadata(metadata: ManUpData): Promise<any> {
+    if (this.storage) {
+      return this.storage.set(STORAGE_KEY + '.manup', JSON.stringify(metadata));
+    } else {
+      throw new Error('Storage not configured');
     }
+  }
 
-    /**
+  /**
      * Returns the branch of the metadata relevant to this platform
      */
-    public getPlatformData(metadata: ManUpData): PlatformData {
-        if (this.platform.is('ios')) {
-            return metadata.ios;
-        }
-        if (this.platform.is('android')) {
-            return metadata.android;
-        }
-        if (this.platform.is('windows')) {
-            return metadata.windows;
-        }
-        throw new Error('Unknown platform');
+  public getPlatformData(metadata: ManUpData): PlatformData {
+    if (this.platform.is('ios')) {
+      return metadata.ios;
     }
+    if (this.platform.is('android')) {
+      return metadata.android;
+    }
+    if (this.platform.is('windows')) {
+      return metadata.windows;
+    }
+    throw new Error('Unknown platform');
+  }
 
-
-    /**
+  /**
      * Presents an update alert.
      * 
      * @param type The type of alert to show
@@ -232,94 +230,106 @@ export class ManUpService {
      * 
      * @returns A promise that resolves when this whole thing is over.
      */
-    private presentAlert(type: AlertType, platformData: any): Promise<any> {
-        switch (type) {
-            case AlertType.MANDATORY:
-                return this.presentMandatoryUpdate(platformData);
+  private presentAlert(type: AlertType, platformData: any): Promise<any> {
+    switch (type) {
+      case AlertType.MANDATORY:
+        return this.presentMandatoryUpdate(platformData);
 
-            case AlertType.OPTIONAL:
-                return this.presentOptionalUpdate(platformData);
+      case AlertType.OPTIONAL:
+        return this.presentOptionalUpdate(platformData);
 
-            case AlertType.MAINTENANCE:
-                return this.presentMaintenanceMode();
-        }
+      case AlertType.MAINTENANCE:
+        return this.presentMaintenanceMode();
     }
+  }
 
-    /**
+  /**
      * Displays a maintenance mode alert.
      * 
      * @returns a promise that will never resolve, because the app should not continue
      */
-    presentMaintenanceMode(): Promise<any> {
-        return this.AppVersion.getAppName().then( (name:string) => {
-            return new Promise((resolve, reject) => {
-                let alert = this.alert.create({
-                    enableBackdropDismiss: false,
-                    title: (this.translate) ? this.translate.instant('manup.maintenance.title', {app: name}) : `${name} Unavailable`,
-                    subTitle: (this.translate) ? this.translate.instant('manup.maintenance.text', {app: name}) : `${name} is currently unavailable. Please check back later`,
-                })
-                alert.present();
-            });
-        })
-    }
+  presentMaintenanceMode(): Promise<any> {
+    return this.AppVersion.getAppName().then((name: string) => {
+      return new Promise((resolve, reject) => {
+        let alert = this.alert.create({
+          enableBackdropDismiss: false,
+          title: this.translate
+            ? this.translate.instant('manup.maintenance.title', { app: name })
+            : `${name} Unavailable`,
+          subTitle: this.translate
+            ? this.translate.instant('manup.maintenance.text', { app: name })
+            : `${name} is currently unavailable. Please check back later`
+        });
+        alert.present();
+      });
+    });
+  }
 
-    /**
+  /**
      * Displays a mandatory update alert.
      * 
      * @returns a promise that will never resolve, because the app should not continue
      */
-    presentMandatoryUpdate(platformData: any): Promise<any> {
-        return this.AppVersion.getAppName().then( (name:string) => {
-            return new Promise((resolve, reject) => {
-                let alert = this.alert.create({
-                    enableBackdropDismiss: false,
-                    title: (this.translate) ? this.translate.instant('manup.mandatory.title', {app: name}) : "Update Required",
-                    subTitle: (this.translate) ? this.translate.instant('manup.mandatory.text', {app: name}) : `An update to ${name} is required to continue.`,
-                    buttons: [
-                        {
-                            text: (this.translate) ? this.translate.instant('manup.buttons.update') : 'Update',
-                            handler: () => {
-                                new InAppBrowser(platformData.url, '_system');
-                                return false;
-                            }
-                        }
-                    ]
-                })
-                alert.present();
-            });
+  presentMandatoryUpdate(platformData: any): Promise<any> {
+    return this.AppVersion.getAppName().then((name: string) => {
+      return new Promise((resolve, reject) => {
+        let alert = this.alert.create({
+          enableBackdropDismiss: false,
+          title: this.translate
+            ? this.translate.instant('manup.mandatory.title', { app: name })
+            : 'Update Required',
+          subTitle: this.translate
+            ? this.translate.instant('manup.mandatory.text', { app: name })
+            : `An update to ${name} is required to continue.`,
+          buttons: [
+            {
+              text: this.translate ? this.translate.instant('manup.buttons.update') : 'Update',
+              handler: () => {
+                this.iab.create(platformData.url, '_system');
+                return false;
+              }
+            }
+          ]
         });
-    }
+        alert.present();
+      });
+    });
+  }
 
-    /**
+  /**
      * Displays an optional update alert.
      * 
      * @returns a promise that will resolves if the user selects 'not now'
      */
-    presentOptionalUpdate(platformData: any): Promise<any> {
-        return this.AppVersion.getAppName().then( (name:string) => {
-            return new Promise((resolve, reject) => {
-                let alert = this.alert.create({
-                    enableBackdropDismiss: false,
-                    title: (this.translate) ? this.translate.instant('manup.optional.title', {app: name}) : "Update Available",
-                    subTitle: (this.translate) ? this.translate.instant('manup.optional.text', {app: name}) : `An update to ${name} is available. Would you like to update?`,
-                    buttons: [
-                        {
-                            text: (this.translate) ? this.translate.instant('manup.buttons.later') : 'Not Now',
-                            handler: () => {
-                                resolve();
-                            }
-                        },
-                        {
-                            text: (this.translate) ? this.translate.instant('manup.buttons.update') : 'Update',
-                            handler: () => {
-                                new InAppBrowser(platformData.url, '_system');
-                                return false;
-                            }
-                        }
-                    ]
-                })
-                alert.present();
-            });
+  presentOptionalUpdate(platformData: any): Promise<any> {
+    return this.AppVersion.getAppName().then((name: string) => {
+      return new Promise((resolve, reject) => {
+        let alert = this.alert.create({
+          enableBackdropDismiss: false,
+          title: this.translate
+            ? this.translate.instant('manup.optional.title', { app: name })
+            : 'Update Available',
+          subTitle: this.translate
+            ? this.translate.instant('manup.optional.text', { app: name })
+            : `An update to ${name} is available. Would you like to update?`,
+          buttons: [
+            {
+              text: this.translate ? this.translate.instant('manup.buttons.later') : 'Not Now',
+              handler: () => {
+                resolve();
+              }
+            },
+            {
+              text: this.translate ? this.translate.instant('manup.buttons.update') : 'Update',
+              handler: () => {
+                this.iab.create(platformData.url, '_system');
+                return false;
+              }
+            }
+          ]
         });
-    }
+        alert.present();
+      });
+    });
+  }
 }
